455557b41d08e895b35609aab8368c0c598f8e35
Eric Kow <eric.kow@gmail.com>
2014-11-14 16:24:50 +0000

    antiword on a directory

---
+++ antiword-dir.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# convert directory of word documents to docbook-like xml format
+
+if [ $# -ne 2 ]; then
+    echo >&2 "Usage: $0 inputdir outputdir"
+    exit 1
+fi
+
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+mkdir -p "$OUTPUT_DIR"
+for i in "$INPUT_DIR"/*.doc; do
+    bname=$(basename "$i")
+    antiword -x db "$i" > "$OUTPUT_DIR"/"$bname"
+done
