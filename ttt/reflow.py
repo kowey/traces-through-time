5081d7f5fb9e6f9dd0fcc337884289f1eb0d6617
Eric Kow <eric.kow@gmail.com>
2014-11-20 17:37:37 +0000

    Avoid accidentally reflowing the date line

--- ttt/reflow.py
+++ ttt/reflow.py
@@ -7,7 +7,21 @@ Throw in some sentence segmentation for good measure
 # author: Eric Kow
 # license: Public domain
 
+from .date import read_date
+
+
 def reflow(tokenizer, text):
-    simple = " ".join(text.split())
+    """
+    Reflow a TTT text (ignoring the first word if it looks
+    like a date)
+    """
+    words = text.split()
+    if words and read_date(words[0]):
+        header = [words[0]]
+        body = words[1:]
+    else:
+        header = []
+        body = words
+    simple = " ".join(body)
     sentences = tokenizer.tokenize(simple)
-    return "\n\n".join(sentences)
+    return "\n\n".join(header + sentences)
