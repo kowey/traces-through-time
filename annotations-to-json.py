c2cfbdcac763fc4a030301da2aeb66c9d269bd93
Eric Kow <eric.kow@gmail.com>
2014-12-11 15:42:51 +0000

    annotations-to-json: support GATE outputs
    Needs to be saved 'preserving document formatting'

--- annotations-to-json.py
+++ annotations-to-json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# pylint: disable=invalid-name
+# (invalid module name ok because script)
+# pylint: enable=invalid-name
 
 """
 Given a directory of annotated texts (where interesting
@@ -19,20 +22,35 @@ from ttt.cli import CliConfig, iodir_argparser, generic_main
 _BRACKET_RE = re.compile(r'men')
 
 
-def mk_json(input_dir, output_dir, subpath):
+def mk_converter(input_format):
     """
-    Given input and output dirs, and a subpath within the
-    input dir, read annotations, and save as json records
-    with just an 'origOccurrence' member.
+    Return a function which given input and output dirs,
+    and a subpath within the input dir, read annotations,
+    and save as json records with just an 'origOccurrence'
+    member ::
+
+
+        InputFormat -> (FilePath, FilePath, FilePath)
+                    -> IO ()
     """
-    ifilename = fp.join(input_dir, subpath)
-    ofilename = fp.join(output_dir, subpath)
-    with codecs.open(ifilename, 'r', 'utf-8') as istream:
-        txt = istream.read()
-        matches = re.findall(r'<(.*?)>', txt)
-        jdicts = [{'origOccurrence': x} for x in matches]
-        with open(ofilename, 'wb') as ostream:
-            json.dump(jdicts, ostream)
+    person_re = re.compile(r'<Person>(.*?)</Person>'
+                           if input_format == 'gate' else
+                           r'<(.*?)>')
+    def inner(input_dir, output_dir, subpath):
+        """
+        ::
+
+            (FilePath, FilePath, FilePath) -> IO ()
+        """
+        ifilename = fp.join(input_dir, subpath)
+        ofilename = fp.join(output_dir, subpath)
+        with codecs.open(ifilename, 'r', 'utf-8') as istream:
+            txt = istream.read()
+            matches = person_re.findall(txt)
+            jdicts = [{'origOccurrence': x} for x in matches]
+            with open(ofilename, 'wb') as ostream:
+                json.dump(jdicts, ostream)
+    return inner
 
 
 def main():
@@ -41,8 +59,11 @@ def main():
                     input_description='annotated text files',
                     glob='*')
     psr = iodir_argparser(cfg)
+    psr.add_argument('--format', choices=['human', 'gate'],
+                     default='human',
+                     help='input markup format')
     args = psr.parse_args()
-    generic_main(cfg, mk_json, args)
+    generic_main(cfg, mk_converter(args.format), args)
 
 if __name__ == '__main__':
     main()
