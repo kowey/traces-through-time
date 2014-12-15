f151141db83ada216abcbc9d205cae0256b6dff5
Eric Kow <eric.kow@gmail.com>
2014-12-15 19:59:34 +0000

    Tune the mark-latest script a bit.

--- devel/mark-latest.sh
+++ devel/mark-latest.sh
@@ -14,20 +14,20 @@ popd > /dev/null
 DATA_DIR="$TTT_DIR"/GOLD/latest
 source "$SCRIPT_DIR/lib"
 
-bash "$SCRIPT_DIR/create-ttt-gold-tarball.sh"
-
-mv "$DATA_DIR/unit-tests-${NEW_ROBOT}.txt" "$DATA_DIR/unit-tests-${OLD_ROBOT}.txt"
+#bash "$SCRIPT_DIR/create-ttt-gold-tarball.sh"
 
 for dataset in $DATASETS; do
     dataset_dir="$DATA_DIR/$dataset"
     old="$dataset_dir/json-$OLD_ROBOT"
     new="$dataset_dir/json-$NEW_ROBOT"
+
     if [ ! -e "$new" ]; then
         echo >&2 "There doesn't seem to be any new data."
         echo >&2 "Have you run nimrodel?"
         exit 1
     fi
     rm -rf "$old"
-    cp -R "$new" "$old"
+    mv "$new" "$old"
 done
 
+mv "$DATA_DIR/unit-tests-${NEW_ROBOT}.txt" "$DATA_DIR/unit-tests-${OLD_ROBOT}.txt"
